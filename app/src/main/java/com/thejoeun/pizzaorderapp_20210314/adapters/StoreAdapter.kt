353f7f1cc6a5f38deb0c86a3c44e9615ec109128
최윤정 <jjjung1003@naver.com>
2021-03-14 12:20:41 +0900

    가게 로고 Glide로 불러내기

--- app/src/main/java/com/thejoeun/pizzaorderapp_20210314/adapters/StoreAdapter.kt
+++ app/src/main/java/com/thejoeun/pizzaorderapp_20210314/adapters/StoreAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.thejoeun.pizzaorderapp_20210314.R
 import com.thejoeun.pizzaorderapp_20210314.datas.Store
 
@@ -32,6 +33,8 @@ class StoreAdapter(
         val storeNameTxt = row.findViewById<TextView>(R.id.storeNameTxt)
 
         storeNameTxt.text = storeData.storeName
+
+        Glide.with(mContext).load(storeData.logoUri).into(logoImg)
 
         return row
     }
