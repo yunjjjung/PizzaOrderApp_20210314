e5cd881c26cbf494aeab2fc078ad60ea6dfa1e8e
최윤정 <jjjung1003@naver.com>
2021-03-14 12:49:40 +0900

    가게 상세 데이터 작업 표시

--- app/src/main/java/com/thejoeun/pizzaorderapp_20210314/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/thejoeun/pizzaorderapp_20210314/ViewStoreDetailActivity.kt
@@ -2,7 +2,9 @@ package com.thejoeun.pizzaorderapp_20210314
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.thejoeun.pizzaorderapp_20210314.datas.Store
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : BaseActivity() {
 
@@ -21,6 +23,11 @@ class ViewStoreDetailActivity : BaseActivity() {
     override fun setValues() {
 
         mStoreData = intent.getSerializableExtra("store") as Store
+
+        nameTxt.text = mStoreData.storeName
+        phoneNumTxt.text = mStoreData.phoneNum
+
+        Glide.with(mContext).load(mStoreData.logoUri).into(logoImg)
 
     }
 
